536854a80c01989aec03b1344bfed69aa272e8ed
Lily Jordan <None>
2024-08-04 15:22:24 -0400

    comment

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 
+# TODO: this can also be applied to arbitrary-dimensional tensors, but might produce an unexpected result, softmaxing
+# the whole tensor (to add to 1) rather than applying softmax to each row. Does that need fixing? Probably either
+# rename `vec` to clarify that any tensor would work, or error if the dimensions aren't 1
 def softmax(vec):
     sum_of_exps = np.sum(np.exp(vec))
     return np.exp(vec) / sum_of_exps
